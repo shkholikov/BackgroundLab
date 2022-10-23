a126120e5b6b730565dcc95de0dd6909ea0039cc
shkholikov <imshakhzod@gmail.com>
2022-10-23 19:58:17 +0200

    Page prototype is finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { Container } from "@mui/system";
-import { AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, Grid, IconButton, Link, Toolbar, Typography } from "@mui/material";
+import {
+	AppBar,
+	BottomNavigation,
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	createTheme,
+	Grid,
+	Link,
+	responsiveFontSizes,
+	ThemeProvider,
+	Toolbar,
+	Typography
+} from "@mui/material";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 
-import MenuIcon from "@mui/icons-material/Menu";
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
-// 👋🏻 Say Hi 🧑🏻‍💻 Shakhzod Kholikov
 function App() {
 	const arr = ["asdasd1", "asfafs2", "asdasd1", "asfafs2", "asdasd1", "asfafs2", "asdasd1", "asfafs2"];
+
 	return (
 		<>
-			<AppBar position="static" color="transparent">
+			<AppBar position="sticky" color="default">
 				<Container>
-					<Toolbar>
+					<Toolbar sx={{ flexDirection: { xs: "column", md: "row" } }}>
 						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
 							BackgroundLab📱
 						</Typography>
@@ -27,18 +44,23 @@ function App() {
 			<Container>
 				<Grid container justifyContent="center" alignItems="center">
 					<Grid item my={"3%"}>
-						<Typography variant="h3" align="center">
-							Welcome to the BackgroundLab📱
-						</Typography>
-						<Typography variant="h4" align="center">
-							Do what makes you happy🎗
-						</Typography>
+						<ThemeProvider theme={theme}>
+							<Typography variant="h4" align="center" sx={{ fontWeight: "600" }}>
+								Welcome to the BackgroundLab📱
+							</Typography>
+							<Typography variant="h5" align="center">
+								Do what makes you happy🎗
+							</Typography>
+							<Typography variant="body1" align="center">
+								BackgroundLab📱 is the place where I can share all my photos. Feel free to visit and download/share/like🖤
+							</Typography>
+						</ThemeProvider>
 					</Grid>
 				</Grid>
-				<Grid container justifyContent="center" alignItems="center" spacing={12}>
+				<Grid container justifyContent="center" alignItems="center" spacing={12} mb={"5%"}>
 					{arr.map((item) => (
 						<Grid item>
-							<Card sx={{ maxWidth: 300 }}>
+							<Card sx={{ minWidth: 300 }}>
 								<CardMedia
 									component="img"
 									alt="green iguana"
@@ -46,7 +68,7 @@ function App() {
 									src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.peerspace.com%2Fresources%2Fphotography-awesome-hobby%2F&psig=AOvVaw3Swbb4GBKXpyF-gLA_fYjz&ust=1666628442376000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCLjz-_Pg9voCFQAAAAAdAAAAABAJ"
 								/>
 								<CardContent>
-									<Typography gutterBottom variant="h5" component="div">
+									<Typography gutterBottom variant="h6" component="div">
 										BackgroundLab-{item}
 									</Typography>
 								</CardContent>
@@ -59,6 +81,16 @@ function App() {
 					))}
 				</Grid>
 			</Container>
+			<BottomNavigation
+				showLabels
+				sx={{
+					backgroundColor: "#f5f5f5",
+					color: "rgba(0, 0, 0, 0.87)",
+					boxShadow: "0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);"
+				}}
+			>
+				<BottomNavigationAction sx={{ fontWeight: "500" }} label="🔗Join my Telegram Channel" value="recents" />
+			</BottomNavigation>
 		</>
 	);
 }
